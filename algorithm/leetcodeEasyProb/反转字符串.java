80903fe10671c867649d957512e2949db76c1fc9
lqt1401737962 <1401737962@qq.com>
2019-12-23 17:23:17 +0800

    算法

---
+++ algorithm/leetcodeEasyProb/反转字符串.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class 反转字符串 {
+    public void reverseCharArray(String s){
+        if (s==null || s.length()<2){
+            return ;
+        }
+        char[] chars = s.toCharArray();
+        int start =-1;
+        int end =chars.length;
+        while (++start < --end){
+            swap(chars,start,end);
+        }
+    }
+    public void swap(char[] chars,int start,int end){
+        char temp = chars[start];
+        chars[start] =chars[end];
+        chars[end] =temp;
+    }
+}
